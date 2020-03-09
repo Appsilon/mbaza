7a96eb83c7da738c4559017d77c28661ee545ce7
Wojtek Dziwulski <wojtek@nomagic.ai>
2020-03-09 15:57:18 +0100

    Add a basic navbar at the top

--- app/containers/HomePage.tsx
+++ app/containers/HomePage.tsx
@@ -1,6 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Alignment, Button } from '@blueprintjs/core';
+
 import Home from '../components/Home';
+import routes from '../constants/routes.json';
 
 export default function HomePage() {
-  return <Home />;
+  return (
+    <>
+      <Navbar>
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>Blueprint</Navbar.Heading>
+          <Navbar.Divider />
+          <Link to={routes.COUNTER}>
+            <Button className="bp3-minimal" icon="home" text="Classify" />
+          </Link>
+          <Link to={routes.COUNTER}>
+            <Button className="bp3-minimal" icon="document" text="Explore" />
+          </Link>
+        </Navbar.Group>
+      </Navbar>
+      <Home />
+    </>
+  );
 }
