38986d0d111ce6de7e3696ca6d6653d9349777df
Wojtek Dziwulski <wojtek@nomagic.ai>
2020-03-10 20:09:02 +0100

    Bring the navbar back

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import { Navbar, Alignment } from '@blueprintjs/core';
 import { routes } from './constants/RoutesType';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
 import ExplorePage from './containers/ExplorePage';
 import ClassifyPage from './containers/ClassifyPage';
-
 import Sidebar from './components/Sidebar';
 
 export default function Routes() {
   return (
     <App>
+      <Navbar>
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>MBaza</Navbar.Heading>
+        </Navbar.Group>
+      </Navbar>
       <div style={{ display: 'flex', flexDirection: 'row' }}>
         <Sidebar routes={routes} />
         <Switch>
