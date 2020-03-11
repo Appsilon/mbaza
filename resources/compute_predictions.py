c53242a70e97ce0243aad9dd24242a3e0cb5fce0
Wojtek Dziwulski <wojtek@nomagic.ai>
2020-03-11 16:17:06 +0100

    Rename the python functions to make it clear they are mocks

--- resources/compute_predictions.py
+++ resources/compute_predictions.py
@@ -6,14 +6,14 @@ import argparse
 def do_prediction(file_path):
     time.sleep(2)
 
-def get_filenames(directory):
+def get_filenames_mock(directory):
     print("Files in the directory {}:".format(directory))
     print(os.listdir(directory))
     return ["img_{}.png".format(x) for x in random.sample(range(1, 100), 10)]
 
-def compute_predictions(directory):
+def compute_predictions_mock(directory):
     print("Will now start computing predictions for images in {} \n".format(directory))
-    filenames = get_filenames(directory)
+    filenames = get_filenames_mock(directory)
     for i, filename in enumerate(filenames):
         print("{}/{} {}".format(i+1, len(filenames), "- "*15))
         print("     Now computing predictions for {}...".format(filename))
@@ -27,7 +27,7 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('--directory', '-d', type=str)
     args = parser.parse_args()
-    compute_predictions(args.directory)
+    compute_predictions_mock(args.directory)
 
 if __name__ == "__main__":
     main()
