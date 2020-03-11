0bf597c14dff0fa90a6040be92e618ed86d12232
Tomasz <tomek.dziopa@gmail.com>
2020-03-11 13:35:54 +0100

    add more paths with the source

--- models/serengeti/hooks/hook-torchvision.py
+++ models/serengeti/hooks/hook-torchvision.py
@@ -1,6 +1,5 @@
 from PyInstaller.utils.hooks import get_package_paths, is_darwin
 
-if is_darwin:
-    datas = [(get_package_paths('torchvision')[1], "site-packages/torchvision")]
-else:
-    datas = [(get_package_paths('torchvision')[1], "torchvision")]
+datas = [
+    (get_package_paths('torchvision')[1], "site-packages/torchvision"),
+    (get_package_paths('torchvision')[1], "torchvision")]
