768f8011cba951260f5c4cbcc90708d44bf770a9
Tomasz <tomek.dziopa@gmail.com>
2020-03-11 12:24:41 +0100

    add osx exception on path included for torchvision jit

--- models/serengeti/hooks/hook-torchvision.py
+++ models/serengeti/hooks/hook-torchvision.py
@@ -1,3 +1,6 @@
-from PyInstaller.utils.hooks import copy_metadata, get_package_paths
+from PyInstaller.utils.hooks import get_package_paths, is_darwin
 
-datas = [(get_package_paths('torchvision')[1], "torchvision")]
+if is_darwin:
+    datas = [(get_package_paths('torchvision')[1], "site-packages/torchvision")]
+else:
+    datas = [(get_package_paths('torchvision')[1], "torchvision")]
