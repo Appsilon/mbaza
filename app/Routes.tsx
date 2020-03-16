024c388ab6716fd0cf27ae6888362bc8bf3659be
Marek Rogala <marek@appsilon.com>
2020-03-16 14:27:57 +0100

    Refactor navbar and sidebar to App

--- app/Routes.tsx
+++ app/Routes.tsx
@@ -1,59 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
 
-import { Navbar, Alignment, Icon, Intent, Button } from '@blueprintjs/core';
-
-import { IconNames } from '@blueprintjs/icons';
-
 import { routes } from './constants/RoutesType';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from './containers/CounterPage';
 import ExplorePage from './containers/ExplorePage';
 import ClassifyPage from './containers/ClassifyPage';
-import Sidebar from './components/Sidebar';
 
 export default function Routes() {
   return (
     <App>
-      <Navbar className="bp3-dark">
-        <Navbar.Group align={Alignment.LEFT}>
-          <Navbar.Heading>
-            <Icon
-              icon={IconNames.MAP}
-              iconSize={Icon.SIZE_LARGE}
-              intent={Intent.PRIMARY}
-            />
-          </Navbar.Heading>
-          <Navbar.Heading>
-            <strong>Mbaza</strong>
-          </Navbar.Heading>
-          <Navbar.Divider />
-          <span>AI for species discovery</span>
-        </Navbar.Group>
-
-        <Navbar.Group align={Alignment.RIGHT}>
-          <Button
-            className="bp3-minimal"
-            icon="projects"
-            text="Projects"
-            style={{ color: '#fff' }}
-          />
-          <Navbar.Divider />
-          <Button className="bp3-minimal" icon="user" />
-          <Button className="bp3-minimal" icon="notifications" />
-          <Button className="bp3-minimal" icon="cog" />
-        </Navbar.Group>
-      </Navbar>
-      <div style={{ display: 'flex', flexDirection: 'row' }}>
-        <Sidebar routes={routes} />
-        <Switch>
-          <Route path={routes.CLASSIFY.path} component={ClassifyPage} />
-          <Route path={routes.EXPLORE.path} component={ExplorePage} />
-          <Route path={routes.COUNTER.path} component={CounterPage} />
-          <Route path={routes.HOME.path} component={HomePage} />
-        </Switch>
-      </div>
+      <Switch>
+        <Route path={routes.CLASSIFY.path} component={ClassifyPage} />
+        <Route path={routes.EXPLORE.path} component={ExplorePage} />
+        <Route path={routes.COUNTER.path} component={CounterPage} />
+        <Route path={routes.HOME.path} component={HomePage} />
+      </Switch>
     </App>
   );
 }
--- app/containers/App.tsx
+++ app/containers/App.tsx
@@ -1,4 +1,9 @@
 import React, { ReactNode } from 'react';
+import { Navbar, Alignment, Icon, Intent, Button } from '@blueprintjs/core';
+import { IconNames } from '@blueprintjs/icons';
+
+import { routes } from '../constants/RoutesType';
+import Sidebar from '../components/Sidebar';
 
 type Props = {
   children: ReactNode;
@@ -6,5 +11,41 @@ type Props = {
 
 export default function App(props: Props) {
   const { children } = props;
-  return <>{children}</>;
+  return (
+    <>
+      <Navbar className="bp3-dark">
+        <Navbar.Group align={Alignment.LEFT}>
+          <Navbar.Heading>
+            <Icon
+              icon={IconNames.MAP}
+              iconSize={Icon.SIZE_LARGE}
+              intent={Intent.PRIMARY}
+            />
+          </Navbar.Heading>
+          <Navbar.Heading>
+            <strong>Mbaza</strong>
+          </Navbar.Heading>
+          <Navbar.Divider />
+          <span>AI for species discovery</span>
+        </Navbar.Group>
+
+        <Navbar.Group align={Alignment.RIGHT}>
+          <Button
+            className="bp3-minimal"
+            icon="projects"
+            text="Projects"
+            style={{ color: '#fff' }}
+          />
+          <Navbar.Divider />
+          <Button className="bp3-minimal" icon="user" />
+          <Button className="bp3-minimal" icon="notifications" />
+          <Button className="bp3-minimal" icon="cog" />
+        </Navbar.Group>
+      </Navbar>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <Sidebar routes={routes} />
+        {children}
+      </div>
+    </>
+  );
 }
