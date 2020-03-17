2fff4dcba216c6afc1a88b5744bf57322d1b5ebf
Tomasz <tomek.dziopa@gmail.com>
2020-03-17 17:12:34 +0100

    further tweaks to compute_predictions

--- resources/compute_predictions.py
+++ resources/compute_predictions.py
@@ -3,10 +3,13 @@ import os
 import time
 import random
 import argparse
+import csv
 
+ANIMALS = ['timon', 'pumba', 'simba', 'nala']
 
 def do_prediction(file_path: str):
-    time.sleep(2)
+    time.sleep(1)
+    return {'path': file_path, 'result': random.choice(ANIMALS)}
 
 
 def get_filenames_mock(directory: str):
@@ -22,21 +25,32 @@ def compute_predictions_mock(inpath: str, outpath: str):
     time.sleep(2)
 
     filenames = list(get_filenames_mock(inpath))
+    results = []
     for i, filename in enumerate(filenames):
         print(
             "{}/{}   Now computing predictions for {}...".format(
                 i + 1, len(filenames), filename
             )
         )
-        do_prediction(file_path=os.path.join(inpath, filename))
+        results.append(do_prediction(file_path=os.path.join(inpath, filename)))
+
+    os.mkdir(outpath)
+
+    with open(os.path.join(outpath, 'results.csv'), 'w') as csvfile:
+        fieldnames = ['path', 'result']
+        writer = csv.DictWriter(csvfile, fieldnames = fieldnames)
+        writer.writeheader()
+        for row in results:
+            writer.writerow(row)
+
 
     print("Predictions computed and saved! \n")
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--inpath", "-i", type=str)
-    parser.add_argument("--outpath", "-o", type=str)
+    parser.add_argument("--inpath", "-i", type=str, required=True)
+    parser.add_argument("--outpath", "-o", type=str, required=True)
     args = parser.parse_args()
     compute_predictions_mock(args.inpath, args.outpath)
 
