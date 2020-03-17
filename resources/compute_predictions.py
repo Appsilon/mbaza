c6dcdd88733b7af28d86e63ffc39363a690ad88a
Tomasz <tomek.dziopa@gmail.com>
2020-03-17 15:17:57 +0100

    in/out parameters for mock model

--- resources/compute_predictions.py
+++ resources/compute_predictions.py
@@ -1,31 +1,43 @@
+#!/usr/bin/env python3
 import os
 import time
 import random
 import argparse
 
-def do_prediction(file_path):
+
+def do_prediction(file_path: str):
     time.sleep(2)
 
-def get_filenames_mock(directory):
+
+def get_filenames_mock(directory: str):
     return os.listdir(directory)
 
-def compute_predictions_mock(directory):
-    print("Will now start computing predictions for images in {} \n".format(directory))
+
+def compute_predictions_mock(inpath: str, outpath: str):
+    print(
+        f"Will now start computing predictions for images from {inpath}, storing results in {outpath} \n"
+    )
     time.sleep(2)
 
-    filenames = get_filenames_mock(directory)
+    filenames = get_filenames_mock(inpath)
     for i, filename in enumerate(filenames):
-        print("{}/{}   Now computing predictions for {}...".format(i+1, len(filenames), filename))
-        do_prediction(file_path=os.path.join(directory+filename))
+        print(
+            "{}/{}   Now computing predictions for {}...".format(
+                i + 1, len(filenames), filename
+            )
+        )
+        do_prediction(file_path=os.path.join(inpath, filename))
 
     print("Predictions computed and saved! \n")
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--directory', '-d', type=str)
+    parser.add_argument("--inpath", "-i", type=str)
+    parser.add_argument("--outpath", "-o", type=str)
     args = parser.parse_args()
-    compute_predictions_mock(args.directory)
+    compute_predictions_mock(args.inpath, args.outpath)
+
 
 if __name__ == "__main__":
     main()
