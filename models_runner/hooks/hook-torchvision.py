a553a57f6aa2c0956422b9a16beb4406971a2290
Marek Rogala <marek@appsilon.com>
2020-06-09 01:08:18 +0200

    Add some packages sources only on win where they seem to be required

--- models_runner/hooks/hook-torchvision.py
+++ models_runner/hooks/hook-torchvision.py
@@ -1,10 +1,11 @@
-from PyInstaller.utils.hooks import get_package_paths, is_darwin
+from PyInstaller.utils.hooks import get_package_paths
+from PyInstaller.compat import is_win
 
 datas = [
     (get_package_paths('torchvision')[1], "site-packages/torchvision"),
     (get_package_paths('torchvision')[1], "torchvision"),
     (get_package_paths('torch')[1], "site-packages/torch"),
-    (get_package_paths('torch')[1], "torch"),
-    # (get_package_paths('mkl_fft')[1], "site-packages/mkl_fft"),
-    # (get_package_paths('mkl_fft')[1], "mkl_fft"),
-]
+    (get_package_paths('torch')[1], "torch")
+] + (
+    [(get_package_paths('mkl_fft')[1], "site-packages/mkl_fft"),
+     (get_package_paths('mkl_fft')[1], "mkl_fft")] if is_win else [])
