51c6200802bc42bceeea93bc8cf59cbc299aaf0a
Kamil Zyla <kamil@appsilon.com>
2020-07-21 22:40:56 +0200

    Remove and reimplement broken redux-beacon-electron
    Instead, implement Google Analytics target for `redux-beacon` middleware
    by hand. Works in dev; does not yet in production.

--- app/store/analyticsMiddleware.ts
+++ app/store/analyticsMiddleware.ts
@@ -1,19 +1,9 @@
 import { LOCATION_CHANGE } from 'connected-react-router';
-import { createMiddleware, EventsMap, EventsMapper } from 'redux-beacon';
+import { createMiddleware, EventsMap } from 'redux-beacon';
 import { trackPageView } from '@redux-beacon/google-analytics';
 import OfflineWeb from '@redux-beacon/offline-web';
 import logger from '@redux-beacon/logger';
-
-import {
-  createElectronGoogleAnalyticsTarget,
-  actionMetaEventMapper
-} from 'redux-beacon-electron';
-
-interface FSA {
-  type: string;
-  payload: any;
-  meta?: any;
-}
+import Analytics from 'electron-ga';
 
 const eventsMap: EventsMap = {
   [LOCATION_CHANGE]: trackPageView(action => ({
@@ -21,27 +11,32 @@ const eventsMap: EventsMap = {
   }))
 };
 
-const eventsMapper = (action: FSA) => {
-  if (action.type in eventsMap) {
-    return eventsMap[action.type];
-  }
-  return actionMetaEventMapper(action as FSA);
-};
+// Meaningless for us, but required by Google Analytics Measurement Protocol:
+// https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#dh
+const HOST = 'electron';
+const analytics = new Analytics('UA-167871460-1');
+function analyticsTarget(events: any) {
+  events.forEach((event: any) => {
+    if (event.hitType === 'pageview') {
+      // TODO: Also send `event.timeSaved` if present (happens when events are
+      // retrieved from offline storage).
+      analytics.send('pageview', { dh: HOST, dp: event.page });
+    } else {
+      console.warn(`Unexpected event.hitType: ${event.hitType}`);
+    }
+  });
+}
 
-const isConnected = (state: { isConnected: boolean }) => navigator.onLine; //state.isConnected;
-// Pass the connectivity selector
-const offlineStorage = OfflineWeb(isConnected);
-
-const electronTarget = createElectronGoogleAnalyticsTarget({
-  ua: 'UA-167871460-1'
-});
-const gaMiddleware = createMiddleware(
-  eventsMapper as EventsMapper,
-  electronTarget,
-  {
-    logger,
-    offlineStorage
-  }
+const offlineStorage = OfflineWeb(
+  // TODO: Understand why using `navigator.online` seems to work. The docs
+  // of `offline-web` state, that that the connectivity flag should be stored
+  // in app state:
+  // https://github.com/rangle/redux-beacon/blob/cad24678a7be400cb0ba2f5e6d57f47945965f50/docs/extensions/offline-web.md
+  (state: { isConnected: boolean }) => navigator.onLine // state.isConnected
 );
 
+const gaMiddleware = createMiddleware(eventsMap, analyticsTarget, {
+  logger,
+  offlineStorage
+});
 export default gaMiddleware;
