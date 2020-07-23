6442f415c40c1897824298e918d34aacba16689e
Kamil Zyla <kamil@appsilon.com>
2020-07-23 15:17:21 +0200

    Enable Google Analytics middleware in production

--- app/store/configureStore.prod.ts
+++ app/store/configureStore.prod.ts
@@ -4,11 +4,12 @@ import { createHashHistory } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 import createRootReducer from '../reducers';
 import { Store, classifierStateType } from '../reducers/types';
+import gaMiddleware from './analyticsMiddleware';
 
 const history = createHashHistory();
 const rootReducer = createRootReducer(history);
 const router = routerMiddleware(history);
-const enhancer = applyMiddleware(thunk, router);
+const enhancer = applyMiddleware(thunk, router, gaMiddleware);
 
 function configureStore(initialState?: classifierStateType): Store {
   return createStore(rootReducer, initialState, enhancer);
