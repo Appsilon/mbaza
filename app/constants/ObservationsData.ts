8611d90b8f76f7620721f19302268d2ba8bf7ee2
Kamil Zyla <kamil@appsilon.com>
2021-01-20 09:12:20 +0100

    Add more WCS columns to the Observation type

--- app/constants/ObservationsData.ts
+++ app/constants/ObservationsData.ts
@@ -1,19 +1,31 @@
 type Observation = {
+  // WCS columns
+  project_id: string;
+  deployment_id: string;
   location: string;
+  identified_by: string;
+  wi_taxon_id: string;
+  class: string;
+  order: string;
+  family: string;
+  genus: string;
+  species: string;
+  common_name: string;
+  uncertainty: number;
+  // Metedata columns
+  station: string;
+  check: string;
+  camera: string;
   date: string;
+  coordinates_long: number;
+  coordinates_lat: number;
+  // Score columns
   pred_1: string;
   score_1: number;
   pred_2: string;
   score_2: number;
   pred_3: string;
   score_3: number;
-  station: string;
-  check: string;
-  camera: string;
-  coordinates_lat: number;
-  coordinates_long: number;
-  project_id: string;
-  deployment_id: string;
 };
 
 type ObservationsData = {
