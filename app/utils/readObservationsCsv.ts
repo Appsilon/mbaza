81163a66e3e9a9588bbd5a36ed13903a26687a94
Kamil Zyla <kamil@appsilon.com>
2021-10-11 21:08:02 +0200

    refactor: Use d3-dsv directly to read observations CSV

--- app/utils/readObservationsCsv.ts
+++ app/utils/readObservationsCsv.ts
@@ -1,6 +1,10 @@
-import { csv } from 'd3-fetch';
+import { promises as fsPromises} from 'fs';
+import { csvParse } from 'd3-dsv';
+
+const { readFile } = fsPromises;
 
 export default async function readObservationsCsv(filePath: string): Promise<Observation[]> {
-  const data = await csv(filePath);
-  return (data as unknown) as Observation[];
+  const file = await readFile(filePath);
+  let data = csvParse(file.toString());
+  return data as unknown as Observation[];
 }
