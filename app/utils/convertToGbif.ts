eb1164bded042cf8920040738d8a966353466d80
Kamil Zyla <kamil@appsilon.com>
2021-10-13 12:47:56 +0200

    feat: Add all columns from Robbie's doc

--- app/utils/convertToGbif.ts
+++ app/utils/convertToGbif.ts
@@ -1,22 +1,39 @@
-import { formatISO } from 'date-fns';
+import { format } from 'date-fns';
 import _ from 'lodash';
 
-// According to https://ipt.gbif.org/manual/en/ipt/2.5/occurrence-data
 function convertEventToGbif(event: Observation[]) {
   const o = event[0]; // Use one of the observations in the event as the representative.
-  const isoDate = formatISO(o.timestamp, { representation: 'date' });
   return {
-    // Required DwC fields
-    occurenceId: [o.project_id, o.station, isoDate].join('__'),
-    basisOfRecord: 'Occurrence',
-    scientificName: '?', // TODO: Check with Robbie what to use here.
-    eventDate: isoDate,
-    // Recommended DwC fields
-    taxonRank: '?', // TODO: Check with Robbie what to use here.
+    occurenceID: [o.project_id, o.station, o.event_id].join('__'),
+    collectionID: '',
+    collectionCode: o.project_id,
     kingdom: 'Animalia',
+    phylum: 'Chordata',
+    class: o.class,
+    order: o.order,
+    family: o.family,
+    genus: o.genus,
+    specificEpithet: o.species,
+    dateIdentified: '', // TODO: Use ISO date of export.
+    identifiedBy: '',
+    continent: '',
+    country: '',
+    // TODO: Add random noise to coordinates (optionally).
     decimalLatitude: o.coordinates_lat,
     decimalLongitude: o.coordinates_long,
-    geodeticDatum: '?' // TODO: Check with Robbie what to use here.
+    basisOfRecord: 'Occurrence',
+    eventDate: format(o.timestamp, 'yyyy-MM-DD'),
+    year: format(o.timestamp, 'yyyy'),
+    month: format(o.timestamp, 'MM'),
+    day: format(o.timestamp, 'DD'),
+    modified: '', // TODO: Use ISO date of export.
+    type: 'StillImage',
+    format: 'JPEG',
+    identifier: '',
+    publisher: '',
+    license: '',
+    rightsHolder: '',
+    scientificName: [o.genus, o.species].join(' ')
   };
 }
 
