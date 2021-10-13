d6988d25e32f3381a9d674d4a527969aca306748
Jakub Chojna <jakub.chojna@appsilon.com>
2021-10-13 15:28:53 +0200

    fix: separate css from sass

--- app/index.tsx
+++ app/index.tsx
@@ -10,6 +10,7 @@ import { configureStore, history } from './store/configureStore';
 import { classifierStateType } from './reducers/types';
 import { statusOnline } from './actions/online';
 import { logError } from './actions/logError';
+import './app.global.css';
 import './styles/app.global.scss';
 
 const initialState: classifierStateType = {
