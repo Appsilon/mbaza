8ff05a3b446ae7ad889017684b0534505d4c4a5f
Kamil Zyla <kamil@appsilon.com>
2021-10-19 11:21:28 +0200

    chore: Bump version to 1.3.0

--- models/runner/inference.py
+++ models/runner/inference.py
@@ -19,7 +19,7 @@ from taxons import taxons_df
 warnings.filterwarnings('ignore')
 
 N_TOP_RESULTS = 3
-APP_VERSION = '1.2.1'
+APP_VERSION = '1.3.0'
 
 def get_images(directory):
     images = [os.path.join(directory, f) for f in get_all_files(directory) if is_image(f)]
