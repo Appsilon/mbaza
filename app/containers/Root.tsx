5155390418f7410a7195b102fe9203bc97892b20
Kamil Zyla <kamil@appsilon.com>
2022-07-08 17:08:16 +0200

    refactor: Drop the remnants of Redux

--- app/containers/Root.tsx
+++ app/containers/Root.tsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import { hot } from 'react-hot-loader/root';
-import { History } from 'history';
-import { Store } from '../reducers/types';
 import Routes from '../Routes';
 
-type Props = {
-  store: Store;
-  history: History;
-};
-
-const Root = ({ store }: Props) => (
-  <Provider store={store}>
-    <HashRouter>
-      <Routes />
-    </HashRouter>
-  </Provider>
+const Root = () => (
+  <HashRouter>
+    <Routes />
+  </HashRouter>
 );
 
 export default hot(Root);
