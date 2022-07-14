ce360d89e8093179cd24148130eddae7edaa0af5
Jakub Chojna <jakub.chojna@appsilon.com>
2022-07-14 14:21:24 +0200

    refactor: extract PathInput to be a separate component

--- app/components/Classifier.tsx
+++ app/components/Classifier.tsx
@@ -16,12 +16,13 @@ import path from 'path';
 import { spawn, ChildProcessWithoutNullStreams } from 'child_process';
 import fs from 'fs';
 import { TFunction } from 'i18next';
-import styles from './Classifier.scss';
 
 import PythonLogViewer from './PythonLogViewer';
 import { openCsvDialog, openDirectoryDialog, saveCsvDialog } from '../utils/fileDialog';
 import { isDev, isLinux, isWin, rootModelsDirectory } from '../utils/environment';
 import MissingModelsMessage from './MissingModelsMessage';
+import PathInput from './PathInput';
+import styles from './Classifier.scss';
 
 const toaster = Toaster.create({});
 
@@ -127,38 +128,6 @@ const computePredictions = (
     });
   }
 };
-
-function PathInput(props: {
-  className: string;
-  placeholder: string;
-  value: string;
-  onChange: (path: string) => void;
-  showDialog: () => Promise<string | undefined>;
-}) {
-  const { className, placeholder, value, onChange, showDialog } = props;
-  return (
-    <div className={`bp3-input-group ${className}`}>
-      <input
-        type="text"
-        className="bp3-input"
-        placeholder={placeholder}
-        value={value}
-        onChange={event => {
-          onChange(event.target.value);
-        }}
-      />
-      <button
-        aria-label="Search"
-        type="submit"
-        className="bp3-button bp3-minimal bp3-intent-primary bp3-icon-search"
-        onClick={async () => {
-          const newValue = await showDialog();
-          if (newValue !== undefined) onChange(newValue);
-        }}
-      />
-    </div>
-  );
-}
 
 export default function Classifier() {
   const { t } = useTranslation();
