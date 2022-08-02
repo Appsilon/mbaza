16db0de96cd397901f422843229ccdae7a64b32c
Kamil Zyla <kamil@appsilon.com>
2022-08-02 16:28:49 +0200

    fix: Disable renderer sandbox mode

--- src/main/main.ts
+++ src/main/main.ts
@@ -78,6 +78,7 @@ const createWindow = async () => {
       preload: app.isPackaged
         ? path.join(__dirname, 'preload.js')
         : path.join(__dirname, '../../.erb/dll/preload.js'),
+      sandbox: false,
     },
   });
 
