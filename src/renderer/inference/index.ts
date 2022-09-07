6555614e843242164b5440cef330852d2ba0ca98
Kamil Zyla <kamil@appsilon.com>
2022-09-07 03:10:38 +0200

    feat: Implement prepareResults()

--- src/renderer/inference/index.ts
+++ src/renderer/inference/index.ts
@@ -1,8 +1,15 @@
 import { csvFormat } from 'd3-dsv';
 import { writeFile } from 'fs/promises';
 import { globby } from 'globby';
+import _ from 'lodash';
+import path from 'path';
 
 import { Model } from '../../common/models';
+import getExifMetadata from './getExifMetadata';
+import getPathMetadata from './getPathMetadata';
+import getStationMetadata from './getStationMetadata';
+import getTopPredictions, { InferenceResult } from './getTopPredictions';
+import getWcsMetadata from './getWcsMetadata';
 
 function listPhotos(dir: string) {
   // Formats supported by `sharp`: https://sharp.pixelplumbing.com/#formats
@@ -14,12 +21,42 @@ function listPhotos(dir: string) {
 export default async function prepareResults(
   inputPath: string,
   outputPath: string,
-  _stationsCsvPath: string,
-  _model: Model,
-  _projectId: string,
-  _deploymentId: string
+  stationsCsvPath: string,
+  model: Model,
+  projectId: string,
+  deploymentId: string
 ) {
-  const photos = await listPhotos(inputPath);
-  const rows = photos.map((location) => ({ location }));
+  const photosRelative = await listPhotos(inputPath);
+  const photosAbsolute = photosRelative.map((photo) => path.join(inputPath, photo));
+
+  const results: InferenceResult[] = await window.ipc.runInference(model, photosAbsolute);
+  const topPredictions = getTopPredictions(results);
+  const pathMetadata = getPathMetadata(photosRelative);
+  const exifMetadata = await getExifMetadata(photosAbsolute);
+  const stationMetadata = await getStationMetadata(pathMetadata, stationsCsvPath);
+
+  const rows = _.range(photosRelative.length).map((i) => {
+    const date = exifMetadata[i].exif_date || pathMetadata[i].path_date;
+    return {
+      ...getWcsMetadata({
+        projectId,
+        deploymentId,
+        location: photosRelative[i],
+        date,
+        time: exifMetadata[i].exif_time,
+        prediction: topPredictions[i].pred_1,
+        score: topPredictions[i].score_1,
+      }),
+      date,
+      coordinates_long: stationMetadata[i].grid_file_long || exifMetadata[i].exif_gps_long,
+      coordinates_lat: stationMetadata[i].grid_file_lat || exifMetadata[i].exif_gps_lat,
+      ...pathMetadata[i],
+      ...exifMetadata[i],
+      ...stationMetadata[i],
+      ...topPredictions[i],
+      ...results[i],
+      label: topPredictions[i].pred_1,
+    };
+  });
   await writeFile(outputPath, csvFormat(rows));
 }
